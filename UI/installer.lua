f227d64e2b8214f686b3fb408089ca137643694a
myselfolli <oliver@laing.online>
2026-01-04 05:18:22 +0100

    Add installer script

---
+++ UI/installer.lua
@@ -0,0 +1,16 @@
+-- install.lua
+local base = "https://raw.githubusercontent.com/myselfolli/cc-atm10/master/"
+
+local files = {
+  "ui/element.lua",
+  "ui/container.lua",
+  "ui/button.lua",
+  "ui/label.lua",
+  "ui/ui.lua",
+}
+
+for _, file in ipairs(files) do
+  shell.run("wget", base .. file, file)
+end
+
+print("Installed UI library")
